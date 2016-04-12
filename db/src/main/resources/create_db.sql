c46bc177424a492e78c4a8ecd1df88fbaee35bfd
Vladimir Filippov <vladimir_filippov@epam.com>
2016-04-12 17:15:18 +0300

    added integration with FlyWay and initial migration script

---
+++ db/src/main/resources/create_db.sql
@@ -0,0 +1,16 @@
+﻿
+
+CREATE USER shop WITH password '<placeholder>';
+
+CREATE DATABASE shopdb
+  WITH OWNER = shop
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'English_United States.1252'
+       LC_CTYPE = 'English_United States.1252'
+       CONNECTION LIMIT = -1;
+
+--GRANT ALL privileges ON DATABASE shopdb TO shop;
+
+CREATE SCHEMA AUTHORIZATION shop; -- schema shop for user shop
+
---
+++ db/src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE product (
+  id SERIAL,
+  name VARCHAR(512) NOT NULL,
+  CONSTRAINT product_pk PRIMARY KEY (id)
+);
+
+COMMENT ON TABLE product IS 'Contains info about a product';
